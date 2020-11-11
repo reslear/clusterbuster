64ac8a13cf86a18579071d187a53ca1b13325b90
reslear <reslear@gmail.com>
2020-11-11 03:46:17 +0300

    temp 2: working

--- example/express.ts
+++ example/express.ts
@@ -1,3 +1,4 @@
+import { TileInput } from './../lib/types/TileInput';
 require('dotenv').config();
 const express = require('express');
 const uuid = require('uuid');
@@ -12,15 +13,18 @@ const { TileServer } = require('../dist');
 TileServer({
   maxZoomLevel,
   attributes: ['status', 'speed'],
+  debug: true,
   filtersToWhere: (filters = { status: undefined, speed: undefined }) => {
     // You are responsible for protecting against SQL injection in this function. Because there are many ways to filter, it depends on the filter type on how to approach this.
+
+    console.log('filters', filters);
 
     // For example a number can be safely used by passing it thorugh parseFloat
     const whereStatements = [];
     if (filters.status && ['busy', 'free'].includes(filters.status)) {
       whereStatements.push(`status = '${filters.status}'`);
     }
-    if (filters.speed && ['slow', 'fast'].includes(filters.speed)) {
+    if (filters.speed && ['slow', 'fast', 'turbo'].includes(filters.speed)) {
       whereStatements.push(`speed = '${filters.speed}'`);
     }
     return whereStatements;
@@ -36,25 +40,23 @@ TileServer({
   });
   app.get('/points/:z/:x/:y/tile.mvt', (req, res) => {
     req.id = uuid.v4();
-    console.time(req.id);
+
     server({
       z: req.params.z,
       x: req.params.x,
       y: req.params.y,
-      query: req.query,
+      queryParams: req.query,
       extent: 4096,
       bufferSize: 256,
       table,
       geometry,
       id: req.id,
-    })
+    } as TileInput<{}>)
       .then((result) => {
         res.setHeader('Content-Type', 'application/x-protobuf');
         res.setHeader('Content-Encoding', 'gzip');
-        console.time('send' + req.id);
+
         res.status(200).send(result);
-        console.timeEnd('send' + req.id);
-        console.timeEnd(req.id);
       })
       .catch((e) => {
         res.status(400).send('Oops');
